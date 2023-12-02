953318e783cbbf61113c787ff116ebf28a442e69
laolarou <luolinrui726@outlook.com>
2023-12-01 23:55:15 -0800

    add helper function

---
+++ Hive.Both.General/Helpers/SessionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Hive.Codec.Abstractions;
+using Hive.Network.Shared.Session;
+using System.IO;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Hive.Both.General.Helpers
+{
+    public static class SessionExtensions
+    {
+        public static async Task SendAsync<T>(
+            this AbstractSession session,
+            IPacketCodec codec,
+            T message,
+            CancellationToken cancellationToken)
+        {
+            using var ms = new MemoryStream();
+            codec.Encode(message, ms);
+            await session.SendAsync(ms, cancellationToken);
+        }
+    }
+}
